67adac65651857bfe192081568d4cc37d6d72182
Josh Grewal <87951727+Joshveer@users.noreply.github.com>
2026-08-18 12:18:59 -0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+
+        int l = 0;
+        int lM = 0;
+        int r = height.size() - 1;
+        int rM = 0;
+        int res = 0;
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lM) {
+                    lM = height[l];
+                } else {
+                    res += lM - height[l];
+                }
+                l++;
+            } else {
+                if (height[r] >= rM) {
+                    rM = height[r];
+                } else {
+                    res += rM - height[r];
+                }
+                r--;
+            }
+        }
+        return res;
+    }
+};
